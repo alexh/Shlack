7f9d719a67743519996bca07185c3f279cf86e03
robzajac <robzajac97@gmail.com>
2017-11-17 17:29:42 -0500

    some client code

--- Client.hs
+++ Client.hs
@@ -7,6 +7,15 @@ module Main where
 import Network.Socket
 import System.IO
 
+-- | IP address of the local host
+local :: HostName
+local = undefined
+
+-- | Start the client given an IP address and a port. The port should
+-- be a string number > 1024
+client :: HostName -> ServiceName -> IO Handle
+client = undefined
+
 -- Main entry point for client.
 main :: IO ()
 main = undefined
