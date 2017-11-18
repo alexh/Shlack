c13d646b941c0149dd98e9b4e239489a6055783e
alexhaynes32 <alhaynes@seas.upenn.edu>
2017-11-17 20:51:57 -0500

    added comments to Client.hs

--- Client.hs
+++ Client.hs
@@ -12,18 +12,21 @@ import Model
 -- Parses user input into a Message.
 parseInput :: String -> Maybe Message
 parseInput = undefined
+    -- Use applicative parsing similar to HW06
 
 -- Serializes messages into a friendly intermediate format to send to server.
 serializeMessage :: Message -> String
 serializeMessage = undefined
+    -- Case analysis on Message constructors
 
 -- Serializes commands into a friendly intermediate format to send to server.
 serializeCommand :: Command -> String
 serializeCommand = undefined
+    -- Case analysis on Command constructors
 
 -- | IP address of the local host
 local :: HostName
-local = undefined
+local = "127.0.0.1"
 
 -- | Start the client given an IP address and a port. The port should
 -- be a string number > 1024
