26e77cb54a3cd3b1ea4b3061077562a6c6cf6d90
robzajac <robzajac97@gmail.com>
2017-12-02 13:15:56 -0500

    server with forking

--- Server.hs
+++ Server.hs
@@ -5,15 +5,19 @@
 module Server where
 
 import qualified Data.Map as M
-import Network
+import qualified Data.ByteString.Char8 as C
+import Control.Concurrent
+import Network hiding (send, sendTo, recv, recvFrom)
+import Network.Socket hiding (send, sendTo, recv, recvFrom)
+import Network.Socket.ByteString
 import Data.Proxy
 import System.IO
 import Model
 
 -- A socket type for either real usage or testing.
 data Socket s where
-    NetSocket :: Server.Socket Network.Socket
-    AbsSocket :: Server.Socket AbstractSocket
+    NetSocket :: Network.Socket -> Server.Socket Network.Socket
+    AbsSocket :: AbstractSocket -> Server.Socket AbstractSocket
 
 -- And here is the abstract socket type for testing.
 data AbstractSocket =
@@ -33,14 +37,23 @@ class Monad m => MonadSocket m s where
   sendTo :: Server.Socket s -> Message -> m ()
 
 -- Concrete MonadSocket instance for actual server.
-instance MonadSocket IO (Server.Socket Network.Socket) where
-    readFrom = undefined
+instance MonadSocket IO Network.Socket where
+    readFrom s' = 
+      let s = getNetSocket s' in
+      do
+        _ <- Network.Socket.accept s
+        byteData <- recv s 1024
+        return (parseMessage (C.unpack byteData))
     sendTo = undefined
+
+getNetSocket :: Server.Socket s -> Network.Socket
+getNetSocket (NetSocket s) = s
+getNetSocket _ = undefined -- todo?
 
 -- Parses String received from Client into a Message.
 -- String has a friendly intermediate format.
 parseMessage :: String -> Message
-parseMessage = undefined
+parseMessage = TextData -- TODO, implement this
 
 -- Evaluate a message sent by this client and update the state.
 -- Has a side effect of writing out to clients the data associated with the message.
@@ -59,9 +72,22 @@ evaluateCommand = undefined
 sendToChannel :: MonadSocket m (Server.Socket s) => Proxy s -> String -> Channel -> m ()
 sendToChannel = undefined
 
+mainLoop :: Network.Socket -> IO ()
+mainLoop s = do
+  _ <- forkIO (do
+    msg <- readFrom (NetSocket s)
+    putStrLn ("ack: " ++ (show msg)))
+  mainLoop s
+
 -- Main entry point for server.
--- main :: IO ()
--- main = undefined
+main :: IO ()
+main = do
+  s <- socket AF_INET Stream defaultProtocol
+  setSocketOption s ReuseAddr 1
+  bind s (SockAddrInet 4040 iNADDR_ANY)
+  listen s 2
+  mainLoop s
+
 -- Open sockets with Clients.
     -- Loop:
         --  Read from socket
