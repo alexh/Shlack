859bf7b86f788192f249edb7dc46db36a4d0a313
robzajac <robzajac97@gmail.com>
2017-12-09 11:46:26 -0500

    todos

--- Server.hs
+++ Server.hs
@@ -89,7 +89,7 @@ evaluateMessage :: (Eq (Server.Socket s), MonadSocket m s) =>
   m (ServerState s)
 evaluateMessage sckt uname msg st =
   case msg of
-      TextData str -> 
+      TextData str -> -- TODO, don't send messages to users being ignored
         let channel = M.lookup uname (userToChannel st) in
         case channel of
           Just c -> do
